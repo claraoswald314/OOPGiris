0f4ada3b1e5e1201544272bd21d507fe5929259f
claraoswald314 <86008186+claraoswald314@users.noreply.github.com>
2023-04-21 09:08:47 +0300

    object oriented programming alıştırmasıdır
    object oriented programming alıştırmasıdır

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        Araba araba1 = new Araba();
+
+       araba1.setModel("Renault");
+
+       System.out.println("arabanın modeli : "+ araba1.getModel());
+
+       araba1.setKapilar(-4);
+
+
+
+    }
+}
